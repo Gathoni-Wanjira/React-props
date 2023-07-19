ce21e9e5d39998b236125ec647e60d673d2d93d6
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-19 14:36:47 +0300

    Passing down props

--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import ItemDescription from './ItemDescription'
 
 function Product({name , description, price}) {
   return (
 //    Consume the product properties from App.js
 // Props destructuring occurs when we use the properties entries as parameters instead of generalizing all of them as just 'props'.
     <div>
-        <h1>{name}</h1>
-        <h2>{description}</h2>
+
+        <ItemDescription name= {name} description={description}/>
         <h3>${price}</h3>
+        
     </div>
+
   )
 }
 
